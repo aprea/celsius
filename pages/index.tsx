40a43702dcf613616253c5efd19ecb5d6716ac61
Chris Aprea <caprea@atlassian.com>
2023-08-20 22:02:43 +1000

    More stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import type { NextPage } from "next";
+import Select from "react-select";
 
 const coinPrices: Record<string, number> = {
   "1INCH": 0.581744108,
@@ -69,48 +70,208 @@ const coinPrices: Record<string, number> = {
   ZUSD: 1,
 };
 
-const Home: NextPage = () => {
-  const [balances, setBalances] = useState<Record<string, number>>({});
+const coinOptions = Object.keys(coinPrices).map((coin) => ({
+  value: coin,
+  label: coin,
+}));
 
-  const handleInputChange = (coin: string, value: number) => {
-    setBalances((prevBalances) => ({ ...prevBalances, [coin]: value }));
+const debounce = (func: Function, wait: number) => {
+  let timeout: NodeJS.Timeout;
+  return function (...args: any) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+};
+
+const Home: NextPage = () => {
+  const [selectedCoins, setSelectedCoins] = useState<
+    Record<string, number | "">
+  >({});
+  const [totalUSD, setTotalUSD] = useState<number>(0);
+  const [addingCoin, setAddingCoin] = useState<boolean>(false);
+  const selectRef = useRef(null);
+  const totalUSDWithIncrease = totalUSD * 1.05;
+
+  // Calculate total USD with debounce
+  const delayedCalculation = useCallback(
+    debounce(() => {
+      const total = Object.entries(selectedCoins).reduce(
+        (total, [coin, balance]) => total + coinPrices[coin] * (balance || 0),
+        0
+      );
+      setTotalUSD(total);
+    }, 500),
+    [selectedCoins]
+  );
+
+  useEffect(() => {
+    delayedCalculation();
+  }, [selectedCoins, delayedCalculation]);
+
+  const handleClear = () => {
+    setSelectedCoins({});
   };
 
-  const calculateTotalUSD = () => {
-    return Object.entries(balances).reduce(
-      (total, [coin, balance]) => total + coinPrices[coin] * balance,
-      0
-    );
+  const handleAddCoin = () => {
+    setAddingCoin(true);
+    setTimeout(() => selectRef.current.focus(), 10); // focus the select input
+  };
+
+  const handleRemoveCoin = (coin: string) => {
+    const { [coin]: removedCoin, ...remainingCoins } = selectedCoins;
+    setSelectedCoins(remainingCoins);
   };
 
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <h1 className="text-2xl font-bold mb-6">Celsius Claim Calculator</h1>
-          {Object.keys(coinPrices).map((coin) => (
-            <div key={coin} className="mb-4">
-              <label
-                htmlFor={coin}
-                className="block text-sm font-medium text-gray-700"
-              >
-                {coin} balance:
-              </label>
+      <div className="w-full max-w-xl mx-auto p-4 rounded-lg shadow-md bg-white">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Celsius Claim Calculator
+        </h1>
+        {addingCoin ? (
+          <Select
+            ref={selectRef}
+            options={coinOptions}
+            className="mb-4"
+            placeholder="Select a coin..."
+            isClearable
+            onBlur={() => setAddingCoin(false)}
+            onChange={({ value }) => {
+              setSelectedCoins({
+                ...selectedCoins,
+                [value]: selectedCoins[value] || "",
+              });
+              setAddingCoin(false);
+            }}
+          />
+        ) : (
+          <button
+            onClick={handleAddCoin}
+            className="mb-4 bg-blue-600 text-white px-3 py-1 rounded-md"
+          >
+            Add Coin
+          </button>
+        )}
+        {Object.keys(selectedCoins).map((coin) => (
+          <div key={coin} className="mb-4">
+            <label
+              htmlFor={coin}
+              className="block text-sm font-medium text-gray-700 mb-1"
+            >
+              {coin} balance:
+            </label>
+            <div className="flex justify-between gap-2">
               <input
                 type="number"
                 id={coin}
-                className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+                value={selectedCoins[coin]}
+                className="p-2 block w-full border border-gray-300 rounded-md flex-1"
                 onChange={(e) =>
-                  handleInputChange(coin, parseFloat(e.target.value))
+                  setSelectedCoins({
+                    ...selectedCoins,
+                    [coin]:
+                      e.target.value === "" ? "" : parseFloat(e.target.value),
+                  })
                 }
               />
+              <button
+                className="bg-red-600 text-white px-3 py-1 rounded-md"
+                onClick={() => handleRemoveCoin(coin)}
+              >
+                Remove
+              </button>
             </div>
-          ))}
+          </div>
+        ))}
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-gray-300 text-gray-700 px-3 py-1 rounded-md"
+            onClick={handleClear}
+          >
+            Clear
+          </button>
           <div className="text-xl font-semibold">
-            Total USD Value: ${calculateTotalUSD().toFixed(2)}
+            Total USD Value:{" "}
+            {new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+            }).format(totalUSD)}
           </div>
         </div>
+      </div>
+      <div className="w-full max-w-xl mx-auto mt-8 p-4 rounded-lg shadow-md bg-white">
+        <h2 className="text-2xl font-bold mb-4 text-center">
+          Estimated Recovery
+        </h2>
+        <p className="mb-1">
+          105% claim:{" "}
+          {new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(totalUSDWithIncrease)}
+        </p>
+        <p className="text-sm text-gray-500 mb-4">
+          Disclaimer: must vote for plan & not opt-out of class-claim.
+        </p>
+        <div className="min-w-full divide-y divide-gray-200">
+          <table className="min-w-full">
+            <thead>
+              <tr>
+                <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Type
+                </th>
+                <th className="px-6 py-3 bg-gray-50 text-right text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Amount
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              <tr>
+                <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                  37% liquid crypto
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 text-right">
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(totalUSDWithIncrease * 0.37)}
+                </td>
+              </tr>
+              <tr>
+                <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                  30% NewCo equity
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 text-right">
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(totalUSDWithIncrease * 0.3)}
+                </td>
+              </tr>
+              <tr>
+                <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 font-bold">
+                  Total estimated recovery
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 text-right font-bold">
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(totalUSDWithIncrease * 0.67)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="w-full max-w-xl mx-auto mt-8 px-4 text-center text-sm text-gray-700">
+        <p>
+          Disclaimer: This calculator is provided for informational purposes
+          only and should not be considered legal, tax, or financial advice.
+          Please consult with a professional advisor before making any financial
+          decisions. We are not responsible for any errors or omissions, or for
+          the results obtained from the use of this information.
+        </p>
       </div>
     </div>
   );
